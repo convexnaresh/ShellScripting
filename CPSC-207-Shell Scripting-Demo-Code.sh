cb1c1506952d697e89d22da1b59e038818ac9fbe
Naresh Adhikari <convex.naresh@gmail.com>
2021-10-06 21:38:22 -0400

    Update CPSC-207-Shell Scripting-Demo-Code.sh
    Added class practice question sets.

--- CPSC-207-Shell Scripting-Demo-Code.sh
+++ CPSC-207-Shell Scripting-Demo-Code.sh
@@ -12,12 +12,15 @@ number=12
 price=100
 echo “$bookname, $number, $price”
 
+
+#===================================
 #Reading user's input in a shell program
 echo “Enter date of purchase: ”
 read varpd
 echo “Purchase date:$varpd”
 echo -p “Enter edition and publisher’s name: ” vared
 
+#===================================
 #Reading  Multiple User inputs in a single variable
 echo “Enter book names each separated by a space”
 read -a varbooks
@@ -26,13 +29,16 @@ echo “${varbooks[1]}”
 echo “${varbooks[2]}”
 echo “${varbooks[3]}”
 
+#===================================
 #Reading User's input through command line (While executing a script)
 echo “Command line values are: ”
 echo “1st input value: $1”
 echo “2nd input value: $2”
 echo “3rd input value: $3”
-echo “0th item is: $0”      
+echo “0th item is: $0”   
 
+
+#===================================
 #Special shell symbols/variables
 echo "$@"
 echo $@
@@ -49,7 +55,7 @@ echo ~+ "~+"
 echo ~- "~-"
 
 
-
+#===================================
 #!/bin/bash
 price=5 #Here ‘=’ is NSBA operator (NO spacing before and after ‘=’, always!)
 quantity=10
@@ -65,7 +71,7 @@ diff=$((price-quantity)) #does this give negative number?
 quot=$((price/quantity)) #does this gives floating number?
 echo “sum,prod,diff,quot =”, $sum, $prod, $diff, $quot
 
-
+#===================================
 #Arithmetic Operators/Incremnting a variable
 myvar=$[$myvar + 1] #outdated, do NOT use
 echo $myvar
@@ -84,6 +90,8 @@ echo $myvar
 ((myvar++)) #post-increment, don't use $
 echo $myvar
 
+
+#===================================
 #Application(s) of Relational Operators
 #!/bin/bash
 price=5 #assignment of variable.
@@ -104,6 +112,8 @@ else
 echo "price less/eq to quantity";
 fi
 
+
+#===================================
 #Applicaition(s) of Logical Operators in Shell
 #!/bin/bash
 a=”true” #assignment of variable.
@@ -128,10 +138,109 @@ else
 echo "both are not true";
 fi
 
+#===================================
+#!/bin/bash
+a=14
+b=67
+bitwiseAND=$(( a&b )) #2
+echo Bitwise AND of a and b is $bitwiseAND
+
+
+bitwiseOR=$(( a|b )) #79
+echo Bitwise OR of a and b is $bitwiseOR
+  
+bitwiseXOR=$(( a^b )) #77
+echo Bitwise XOR of a and b is $bitwiseXOR
+
+
+#Complement
+bitiwiseComplement=$(( ~a )) #-15
+echo Bitwise Compliment of a is $bitiwiseComplement
+  
+leftshift=$(( a<<1 )) #28
+echo Left Shift of a is $leftshift
+  
+rightshift=$(( b>>1 )) #33
+echo Right Shift of b is $rightshift
+
+
+##===================================
+#Applications of floating point arithmetic in Shell
+#!/bin/bash
+price=5
+quantity=10
+#floating point arithmetic operators
+quot=$((price/quantity)) #does this gives floating number?
+fquot=`echo "$price / $quantity" | bc -l` #does give floating point value.
+#OR
+#fquot=$(echo "$price / $quantity" | bc -l) #substitution command/syntax
+
+afquot= `echo - | awk"{print $price / $quantity}"`
+echo $quot, $fquot
+echo $afquot
+
+min=12.45
+val=10.35
+if (("$(echo "${val} < ${min}" | bc)" == 1)); then  
+    echo "minimum value is tested"
+    min=${val} #new min.
+fi
+echo "min value is: $min"
+
+Output:
+0, 0.5
+0.5
+min value is: 10.35
 
 
 
-                         
 
+##===================================
+#Class Practice Problem-1
+#Write a shell script named 'calc.sh' that takes three command line inputs, and performs 
+#as a calculator as below:
+#The first command line argument is a number
+#The second command line argument is an operator
+#The third command line arugment is another number.
+Once given correct inputs, the script should display the result of the operation given 
+through the command line.
+For example, the script can be execuated as below to get desired result:
+  $bash calc.sh 45 + 65
+  Output: 110
 
+  $bash calc.sh 200 \ 10
+  Output: 20
+
+  $bash calc.sh 30 \* 20
+  Output: 600
+
+#Class Practice Problem-2
+# Write a shell script catlog.sh that takes user's 4 different data values (name, age, gpa and salary) on the command line.
+# The script should also classify the user in different age groups and GPA groups as below.
+    #Age Groups:
+    # 0 - 14 child
+    # 15 - 24 youth                      
+    # 25 - 64 adults
+    # 65 + senior
+
+    #GPA Groups:
+    pass: less than 3.0 total GPA
+    cum laude: at least a 3.0 total GPA 
+    magna cum laude: at least a 3.4 total GPA
+    summa cum laude: at least a 3.7 total GPA
+
+#Your program should display user's name, proper age group name and GPA group name according to user's command line inputs.
+
+  #Example:
+  #$./catlog.sh david 12 2.5 0
+  Output:
+  name: david
+  age group: child
+  GPA group: pass
+
+  #$./catlog.sh rohan 42 3.9 42000
+  Output:
+  name: rohan
+  age group: adults
+  GPA group: magna cum laude
 
